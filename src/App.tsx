b26ba9f5399812071ca2323c9ce9ba37fe128616
TaikiCode <78462662+TaikiCode@users.noreply.github.com>
2022-02-13 23:29:56 +0900

    header実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Navbar from './components/navbar/Navbar';
+import Header from './components/header/Header';
+import './App.css';
+
 
 function App() {
   return (
     <>
       <Navbar />
-      <div style={{height: "100vh", backgroundColor: "red"}}></div>
-      <div style={{height: "100vh"}}></div>
+      <Header />
     </>
   );
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import { VFC } from "react"
+import Typed from "react-typed"
 
-const Header = () => {
-    return (
-        <div>
-            
+const Header: VFC = () => {
+  const HEADER_TEXT = "Taiki Okura"
+  const TYPE_CONTENTS = [
+    "Web Design.",
+    "Web Development.",
+    "Artificial Intelligence.",
+    "Data science.",
+  ]
+  return (
+    <div id="home" className="header-wrapper">
+      <div className="main-info">
+        <h1 className="font-weight-bold font-italic">{HEADER_TEXT}</h1>
+        <div className="typed-wrapper mt-4">
+          <Typed
+            className="typed-text"
+            strings={TYPE_CONTENTS}
+            typeSpeed={40}
+            backSpeed={60}
+            loop
+          />
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Header
