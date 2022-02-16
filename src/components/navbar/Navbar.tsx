3445ebc7b05f9ea8ed4931534930d2ebf7332338
TaikiCode <78462662+TaikiCode@users.noreply.github.com>
2022-02-16 10:33:29 +0900

    navbarのレスポンシブ対応

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,11 +1,18 @@
 import { VFC, useState, useEffect } from "react"
-import { MenuIcon } from "@heroicons/react/solid"
+import { FiMenu } from "react-icons/fi"
+import { IoCloseSharp } from "react-icons/io5"
+
+
 import NavItem from "./NavItem"
 import "./styles.scss"
 import { NAV_ITEMS } from "../../contents/navItems"
 
 const Navbar: VFC = () => {
   const [isHeightOver, setIsHeightOver] = useState<boolean>(false)
+
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false)
+  const handleOpen = () => setIsMenuOpen(true)
+  const handleClose = () => setIsMenuOpen(false)
 
   useEffect(() => {
     const scrollAction = () => {
@@ -26,10 +33,45 @@ const Navbar: VFC = () => {
   }, [])
 
   return (
-    <div className={`nav ${!isHeightOver && "active"}`}>
-      <div className="hidden px-2 mx-2 w-full lg:flex lg:justify-center lg:items-center">
-        <div className="flex items-stretch">
-          {NAV_ITEMS.map((item, index) => <NavItem key={index} text={item.text} toPath={item.toPath} offset={item.offset} isHeightOver={isHeightOver}/>)}
+    <div className={`navStyle ${!isHeightOver && "active"}`}>
+      <div className="px-2 mx-2 w-full lg:flex lg:justify-center lg:items-center">
+        <div className="hidden lg:flex items-stretch">
+          {NAV_ITEMS.map((item, index) => (
+            <NavItem
+              key={index}
+              text={item.text}
+              toPath={item.toPath}
+              offset={item.offset}
+              isHeightOver={isHeightOver}
+            />
+          ))}
+        </div>
+        {/* モバイル用 */}
+        <div className="lg:hidden flex pt-2">
+          {isMenuOpen ? (
+            <>
+              <IoCloseSharp
+                className="text-base-100 text-3xl z-50"
+                onClick={handleClose}
+              />
+              <div className="overlay">
+                {NAV_ITEMS.map((item) => (
+                  <div className="mobileItem">
+                    <NavItem
+                      text={item.text}
+                      toPath={item.toPath}
+                      offset={item.offset}
+                    />
+                  </div>
+                ))}
+              </div>
+            </>
+          ) : (
+            <FiMenu
+              className="text-base-100 text-3xl z-50"
+              onClick={handleOpen}
+            />
+          )}
         </div>
       </div>
     </div>
@@ -37,4 +79,3 @@ const Navbar: VFC = () => {
 }
 
 export default Navbar
-
